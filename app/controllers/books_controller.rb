dd4104a65a1120cc857462c1c34969a4b354f793
rymk5947 <164598021+rymk5947@users.noreply.github.com>
2024-05-23 01:25:31 +0000

    [Finish]

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,69 @@
+class BooksController < ApplicationController
+   before_action :ensure_correct_user, only: [:edit, :update]
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+  if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book)
+  else
+      @user = current_user
+      @books = Book.all
+      render :index
+  end
+  end
+
+  def index
+    @book = Book.new
+    @books = Book.all
+    @user = current_user
+  end
+
+  def edit
+  @book = Book.find(params[:id])
+  if current_user != @book.user
+    redirect_to books_path, notice: "他人の投稿は編集できません。"
+  end
+  end
+
+  def show
+    @book = Book.find(params[:id])
+    @user = @book.user
+    @new_book = Book.new
+  end
+
+
+  def destroy
+    @book = Book.find(params[:id])
+    if @book.destroy
+      flash[:notice] = "You have destroyed book successfully."
+    end
+      redirect_to books_path
+  end
+
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
+  end
+
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+  def ensure_correct_user
+    book = Book.find(params[:id])
+    unless book.user_id == current_user.id
+      redirect_to books_path
+    end
+  end
+end
